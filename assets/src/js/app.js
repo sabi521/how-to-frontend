e9b17c390817f9febae9e29fe5abbc6fb89cccab
sarbjeet <sabi521@gmail.com>
2024-07-02 16:26:51 -0400

    home page update

--- assets/src/js/app.js
+++ assets/src/js/app.js
@@ -3,19 +3,34 @@
  * @param {function} fn
  * @return {void}
  */
-function domReady(fn)
-{
-    // If we're early to the party
-    document.addEventListener("DOMContentLoaded", fn);
-    // If late; I mean on time.
-    if (document.readyState === "interactive" || document.readyState === "complete" ) {
-        fn();
-    }
+function domReady(fn) {
+  // If we're early to the party
+  document.addEventListener("DOMContentLoaded", fn);
+  // If late; I mean on time.
+  if (
+    document.readyState === "interactive" ||
+    document.readyState === "complete"
+  ) {
+    fn();
+  }
 }
 
 /**
  * Application entrypoint
  */
 domReady(() => {
-    console.log('The Dom is ready! 🚀');
+  console.log("The Dom is ready! 🚀");
+
+  /**
+   * search bar in the banner
+   */
+  const searchInput = document.getElementById("default-search");
+  const searchButton = document.getElementById("search-icon");
+  searchInput.addEventListener("input", function () {
+    if (searchInput.value.length > 0) {
+      searchButton.classList.add("hidden");
+    } else {
+      searchButton.classList.remove("hidden");
+    }
+  });
 });
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,33 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    mode: 'jit', // activates Just-in-Time mode
-    prefix: '', // adds 'tw-' prefix to the classes
-    content: [
-        './**/*.html',
-        './**/*.{vue,js,ts,jsx,tsx}'
-    ], // update this path if needed, include *.php
-    media: false, // or 'media' or 'class'
-    theme: {
-        extend: {},
+  mode: "jit", // activates Just-in-Time mode
+  prefix: "", // adds 'tw-' prefix to the classes
+  content: ["./**/*.html", "./**/*.{vue,js,ts,jsx,tsx}"], // update this path if needed, include *.php
+  media: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["ui-sans-serif", "system-ui"],
+        roboto: ["Roboto", "sans-serif"],
+        montserrat: ["montserrat", "sans-serif"],
+        nexaheavy: ['"Nexa Heavy"'],
+        nexabold: ['"Nexa Bold"'],
+        nexaregular: ['"Nexa Regular"'],
+        nexalight: ['"Nexa Light"'],
+        kommissarCondensed: ['"Kommissar Condensed"'],
+        ubuntuRegular: ["Ubuntu Regular"],
+        ubuntuLight: ["Ubuntu Light"],
+        ubuntuBold: ["Ubuntu Bold"],
+        ubuntuMedium: ["Ubuntu Medium"],
+      },
+      boxShadow: {
+        "3xl": " 0px 1px 11.3px 0px rgba(39, 49, 60, 0.12);",
+        "4xl": "0px 0px 0px 3px rgba(0, 114, 235, 0.42)",
+      },
     },
-    variants: {
-        extend: {},
-    },
-    plugins: [],
-}
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
