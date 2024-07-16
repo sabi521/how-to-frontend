c7107bf3883bfb9db205967985d05c8d948fd958
sarbjeet <sabi521@gmail.com>
2024-07-16 16:44:56 -0400

    qa changes

--- assets/src/js/app.js
+++ assets/src/js/app.js
@@ -20,6 +20,184 @@ function domReady(fn) {
  */
 domReady(() => {
   console.log("The Dom is ready! 🚀");
+
+  /** Slider for The most popular section */
+
+  // Function to initialize a Splide slider if it exists
+  function initializeSplide(selector, options) {
+    var element = document.querySelector(selector);
+    if (element) {
+      var splide = new Splide(selector, options);
+      splide.mount();
+      return splide;
+    }
+    return null;
+  }
+
+  // Initialize sliders if they exist on the page
+  var splide1 = initializeSplide("#splide1", {
+    type: "loop", // slide loop fade
+    perPage: 4,
+    gap: 40,
+    fixedWidth: 306,
+    pagination: false,
+    arrows: false,
+    easing: "ease",
+  });
+
+  var splide2 = initializeSplide("#splide2", {
+    type: "loop", // slide loop fade
+    perPage: 4,
+    gap: 40,
+    fixedWidth: 306,
+    pagination: false,
+    arrows: false,
+    easing: "ease",
+  });
+
+  var splide3 = initializeSplide("#splide3", {
+    type: "loop",
+    gap: 10,
+    fixedWidth: 326,
+    pagination: false,
+    arrows: false,
+    easing: "ease",
+  });
+
+  // Function to handle slider navigation
+  function handleSliderNavigation(event, direction) {
+    var sliderId = event.currentTarget.getAttribute("data-slider-id");
+    var splide = sliderId === "splide1" ? splide1 : splide2;
+    if (splide) {
+      splide.go(direction);
+    }
+  }
+
+  // Adding event listeners to the SVG buttons if they exist
+  document.querySelectorAll(".slider-prev-btn").forEach(function (button) {
+    button.addEventListener("click", function (event) {
+      handleSliderNavigation(event, "-1");
+    });
+  });
+
+  document.querySelectorAll(".slider-next-btn").forEach(function (button) {
+    button.addEventListener("click", function (event) {
+      handleSliderNavigation(event, "+1");
+    });
+  });
+
+  /**
+   * Tabs for the home page
+   */
+  let tabs = document.querySelectorAll(".tab");
+  let panels = document.querySelectorAll(".tab-panel");
+
+  if (tabs.length > 0 && panels.length > 0) {
+    // Set the first tab and panel as selected by default
+    tabs[0].querySelector("h3").classList.add("selected-heading");
+    tabs[0].querySelector("p").classList.add("selected-paragraph");
+    tabs[0].classList.add("selected-tab");
+    panels[0].classList.add("block");
+
+    tabs.forEach((tab) => {
+      tab.addEventListener("click", () => {
+        let tabTarget = tab.getAttribute("aria-controls");
+
+        // Change the selected tab heading color and border
+        tabs.forEach((t) => {
+          t.querySelector("h3").classList.remove("selected-heading");
+          t.querySelector("p").classList.remove("selected-paragraph");
+          t.classList.remove("selected-tab");
+          t.setAttribute("aria-selected", "false");
+        });
+        tab.querySelector("h3").classList.add("selected-heading");
+        tab.querySelector("p").classList.add("selected-paragraph");
+        tab.classList.add("selected-tab");
+        tab.setAttribute("aria-selected", "true");
+
+        // Show/Hide the content panels
+        panels.forEach((panel) => {
+          let panelId = panel.getAttribute("id");
+          if (tabTarget === panelId) {
+            panel.classList.remove("hidden");
+            panel.classList.add("block");
+          } else {
+            panel.classList.remove("block");
+            panel.classList.add("hidden");
+          }
+        });
+      });
+    });
+  }
+
+  /**
+   * Search bar in the banner
+   */
+  const searchInput = document.getElementById("default-search");
+  const searchButton = document.getElementById("search-icon");
+
+  if (searchInput && searchButton) {
+    searchInput.addEventListener("input", function () {
+      if (searchInput.value.length > 0) {
+        searchButton.classList.add("hidden");
+      } else {
+        searchButton.classList.remove("hidden");
+      }
+    });
+  }
+
+  /**
+   * How to bet search form
+   */
+  function expandSearch() {
+    var headerSearchInput = document.getElementById("headerSearchInput");
+    var headerSearchForm = document.getElementById("headerSearchForm");
+    var headerSearchBtn = document.getElementById("headerSearchBtn");
+    var headerCloseBtn = document.getElementById("headerCloseBtn");
+
+    if (
+      headerSearchForm &&
+      headerSearchForm.classList.contains("max-w-[278px]")
+    ) {
+      headerSearchForm.classList.remove("max-w-[278px]");
+      headerSearchForm.classList.add("max-w-full");
+      headerSearchInput.classList.remove("hidden");
+      headerSearchInput.placeholder = "";
+      headerSearchInput.focus();
+      headerSearchInput.classList.add("pl-14");
+      headerSearchBtn.classList.add("left-5"); // Move search button to the left
+      headerCloseBtn.classList.remove("hidden"); // Show close button
+    }
+  }
+
+  function collapseSearch() {
+    var headerSearchInput = document.getElementById("headerSearchInput");
+    var headerSearchForm = document.getElementById("headerSearchForm");
+    var headerSearchBtn = document.getElementById("headerSearchBtn");
+    var headerCloseBtn = document.getElementById("headerCloseBtn");
+
+    if (headerSearchForm && headerSearchForm.classList.contains("max-w-full")) {
+      headerSearchForm.classList.remove("max-w-full");
+      headerSearchForm.classList.add("max-w-[278px]");
+      headerSearchInput.classList.add("hidden");
+      headerSearchInput.classList.remove("pl-14");
+      headerSearchInput.placeholder = "search"; // Restore the placeholder
+      headerSearchBtn.classList.remove("left-5"); // Move search button back
+      headerCloseBtn.classList.add("hidden"); // Hide close button
+      headerSearchBtn.classList.remove("hidden"); // Show search button
+    }
+  }
+
+  var headerSearchBtn = document.getElementById("headerSearchBtn");
+  var headerSearchInput = document.getElementById("headerSearchInput");
+  var headerCloseBtn = document.getElementById("headerCloseBtn");
+
+  if (headerSearchBtn && headerSearchInput && headerCloseBtn) {
+    headerSearchBtn.addEventListener("click", expandSearch);
+    headerSearchInput.addEventListener("focus", expandSearch);
+    headerCloseBtn.addEventListener("click", collapseSearch);
+  }
+
   /** Search functionality show and hide 404 page */
 
   const customSearchInput = document.getElementById("custom-search-input");
@@ -65,126 +243,15 @@ domReady(() => {
     });
   }
 
+  /**
+   * Dropdown menu
+   */
   const dropdownBtn = document.querySelector(".agent_dropdown_btn");
   const dropdownMenu = document.getElementById("agentDropdown");
 
-  dropdownBtn.addEventListener("click", function () {
-    dropdownMenu.classList.toggle("hidden");
-  });
-
-  /**
-   * search bar in the banner
-   */
-  const searchInput = document.getElementById("default-search");
-  const searchButton = document.getElementById("search-icon");
-
-  if (searchInput && searchButton) {
-    searchInput.addEventListener("input", function () {
-      if (searchInput.value.length > 0) {
-        searchButton.classList.add("hidden");
-      } else {
-        searchButton.classList.remove("hidden");
-      }
+  if (dropdownBtn && dropdownMenu) {
+    dropdownBtn.addEventListener("click", function () {
+      dropdownMenu.classList.toggle("hidden");
     });
   }
-
-  /**
-   * tabs for the home page
-   */
-  let tabs = document.querySelectorAll(".tab");
-  let panels = document.querySelectorAll(".tab-panel");
-
-  // Set the first tab and panel as selected by default
-  tabs[0].querySelector("h3").classList.add("selected-heading");
-  tabs[0].querySelector("p").classList.add("selected-paragraph");
-  tabs[0].classList.add("selected-tab");
-  panels[0].classList.add("block");
-
-  tabs.forEach((tab) => {
-    tab.addEventListener("click", () => {
-      let tabTarget = tab.getAttribute("aria-controls");
-
-      // Change the selected tab heading color and border
-      tabs.forEach((t) => {
-        t.querySelector("h3").classList.remove("selected-heading");
-        t.querySelector("p").classList.remove("selected-paragraph");
-        t.classList.remove("selected-tab");
-        t.setAttribute("aria-selected", "false");
-      });
-      tab.querySelector("h3").classList.add("selected-heading");
-      tab.querySelector("p").classList.add("selected-paragraph");
-      tab.classList.add("selected-tab");
-      tab.setAttribute("aria-selected", "true");
-
-      // Show/Hide the content panels
-      panels.forEach((panel) => {
-        let panelId = panel.getAttribute("id");
-        if (tabTarget === panelId) {
-          panel.classList.remove("hidden");
-          panel.classList.add("block");
-        } else {
-          panel.classList.remove("block");
-          panel.classList.add("hidden");
-        }
-      });
-    });
-  });
-
-  /** Slider for The most popular section */
-
-  var splide1 = new Splide("#splide1", {
-    type: "loop", // slide loop fade
-    perPage: 4,
-    gap: 40,
-    fixedWidth: 306,
-    pagination: false,
-    arrows: false,
-    easing: "ease",
-  });
-  splide1.mount();
-
-  /** Slider for New Articles section */
-
-  var splide2 = new Splide("#splide2", {
-    type: "loop", // slide loop fade
-    perPage: 4,
-    gap: 40,
-    fixedWidth: 306,
-    pagination: false,
-    arrows: false,
-    easing: "ease",
-  });
-  splide2.mount();
-
-  /** Slider for whats new on sportsbook section */
-
-  var splide = new Splide("#splide3", {
-    type: "loop",
-    gap: 10,
-    fixedWidth: 326,
-    pagination: false,
-    arrows: false,
-    easing: "ease",
-  });
-  splide.mount();
-
-  // Function to handle slider navigation
-  function handleSliderNavigation(event, direction) {
-    var sliderId = event.currentTarget.getAttribute("data-slider-id");
-    var splide = sliderId === "splide1" ? splide1 : splide2;
-    splide.go(direction);
-  }
-
-  // Adding event listeners to the SVG buttons
-  document.querySelectorAll(".slider-prev-btn").forEach(function (button) {
-    button.addEventListener("click", function (event) {
-      handleSliderNavigation(event, "-1");
-    });
-  });
-
-  document.querySelectorAll(".slider-next-btn").forEach(function (button) {
-    button.addEventListener("click", function (event) {
-      handleSliderNavigation(event, "+1");
-    });
-  });
 });
